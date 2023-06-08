9f45422debb4ab7c0eb9b2e2ec4ac1edf2f3ff62
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-06-08 14:13:28 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "avancer") {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+    } else if (receivedString == "reculer") {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+    } else if (receivedString == "droite") {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 40)
+    } else if (receivedString == "gauche") {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 40)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+    } else {
+        motor.motorStopAll()
+    }
+})
+radio.setGroup(5)
